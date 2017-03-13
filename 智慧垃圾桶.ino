bbf04ed473143175d657b2cfa2669dedba1edfae
DESKTOP-BNOGFH7\suffee <suffeesu@gmail.com>
2017-03-13 21:17:01 +0800

    MAIN

--- 智慧垃圾桶.ino
+++ 智慧垃圾桶.ino
@@ -1,15 +1,32 @@
+#include <SPI.h>
 #include <Servo.h>
+#include <MFRC522.h>
 #include <Ultrasonic.h>
 #include <SoftwareSerial.h>
-SoftwareSerial HC06(10, 11); // TX, RX
-#define TRIGGER_PIN  4
-#define ECHO_PIN     2
+
+#define TRIGGER_PIN  		4
+#define ECHO_PIN     		2
+#define SS_PIN 		 		53
+#define RST_PIN 	 		A1
+#define COVER_MOTOR_PIN			A0
+
+#define LEFT_MOTOR_ENABLE		3
+#define LEFT_FRONT_MOTOR_PIN	9
+#define LEFT_BACK_MOTOR_PIN		8
+
+#define RIGHT_MOTOR_ENABLE		5
+#define RIGHT_FRONT_MOTOR_PIN	7
+#define RIGHT_BACK_MOTOR_PIN	6
+
+#define LEFT_IR_SENSOR			A2
+#define MIDDLE_IR_SENSOR		A3
+#define RIGHT_IR_SENSOR			A4
+
 Servo myservo;
-#include <SPI.h>
-#include <MFRC522.h>
-#define SS_PIN 53
-#define RST_PIN A1
+SoftwareSerial HC06(10, 11); // TX, RX
 MFRC522 rfid(SS_PIN, RST_PIN);
+Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
+
 int pos = 90;
 int enableA = 3;
 int pinA1 = 9;
@@ -29,26 +46,34 @@ int i1 = A2;     //sensor1
 int i2 = A3;
 int i3 = A4;    //sensor2
 int s1,s2,s3;
-Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
+
 
 void setup()
 {
-  myservo.attach(A0);
-  Serial.begin(9600);
-  SPI.begin(); 
-  rfid.PCD_Init();
-  HC06.begin(9600);
-  pinMode(enableA, OUTPUT);
-  pinMode(pinA1, OUTPUT);
-  pinMode(pinA2, OUTPUT);
+	SPI.begin();
+	rfid.PCD_Init();
+	HC06.begin(9600);
+	Serial.begin(9600);
+	myservo.attach(COVER_MOTOR_PIN);
+  
+	//Initialize Left Side Mortors
+  
+	pinMode(LEFT_MOTOR_ENABLE	,OUTPUT);
+	pinMode(LEFT_BACK_MOTOR_PIN	,OUTPUT);
+	pinMode(LEFT_FRONT_MOTOR_PIN,OUTPUT);
  
-  pinMode(enableB, OUTPUT);
-  pinMode(pinB1, OUTPUT);
-  pinMode(pinB2, OUTPUT);
-
-  pinMode(i1, INPUT);
-  pinMode(i2, INPUT);
-  pinMode(i3, INPUT);
+	//Initialize Right Side Motors
+ 
+	pinMode(RIGHT_MOTOR_ENABLE	,OUTPUT);
+	pinMode(RIGHT_BACK_MOTOR_PIN,OUTPUT);
+	pinMode(RIGHT_FRONT_MOTOR_PIN,OUTPUT);
+	
+	//Initialize IR sensors
+	
+	pinMode(LEFT_IR_SENSOR, INPUT);
+	pinMode(RIGHT_IR_SENSOR, INPUT);
+	pinMode(MIDDLE_IR_SENSOR, INPUT);
+	
 }
 void loop()
 {
